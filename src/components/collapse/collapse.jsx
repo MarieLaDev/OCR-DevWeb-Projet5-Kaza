5b434636dfcda519ccff69e074723690cdcb47bd
MarieLaDev <marie.dehaullon@gmail.com>
2024-09-29 17:45:24 +0200

    Simplification du collapse (même useState pour flèche et open Collaspe

--- src/components/collapse/collapse.jsx
+++ src/components/collapse/collapse.jsx
@@ -2,16 +2,13 @@ import React, {useState} from 'react';
 import './collapse.scss'
 
 function Collapse({type}) {
-  // isOpen <= détermine si le contenu est visible ou non  initialisé à false
+  // isOpen <= détermine si le contenu est visible ou non initialisé à false
   const [isOpen, setIsOpen] = useState(false);
 
-  // useState rotation du bouton collapse
-  const [isRotated, setIsRotated] = useState(false);
 
   // Inverse l'état isOpen et aussi isRotated (pour la flêche) à chaque clic sur la flèche
   const toggleCollapse = () => {
     setIsOpen(!isOpen);
-    setIsRotated(!isRotated);
   };
   // console.log(isOpen + "< isOpen / isRotated >" + isRotated);
 
@@ -28,7 +25,7 @@ function Collapse({type}) {
         data-target={type.id}>
           {type.title}
           <div className="arrow">
-            <i className={`fa-solid fa-chevron-up ${isRotated ? 'openI' : 'closeI'}`} onClick={toggleCollapse}></i>
+            <i className={`fa-solid fa-chevron-up ${isOpen ? 'openI' : 'closeI'}`} onClick={toggleCollapse}></i>
           </div>
         </div>
 
