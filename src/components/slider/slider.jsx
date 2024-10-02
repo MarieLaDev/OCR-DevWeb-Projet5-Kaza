5798a38bfb4f7856a85ed32756428166bb58c35c
MarieLaDev <marie.dehaullon@gmail.com>
2024-10-02 10:33:11 +0200

    modif chemin icones prev-next

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './_slider.scss'
 import prevIcon from '../../assets/images/prev.svg';
-import nextIcon from '../../assets/images/next.svg';
+import nextIcon from '../../assets/images/prev.svg';
 
 
 function Slider({pictures, title}) {
